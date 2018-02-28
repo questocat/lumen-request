3187cb8404f4f6bfb98e4b426571b99c61c56244
zhengchaopu <zhengchaopu@gmail.com>
2018-02-28 10:29:56 +0800

    Add form request make command

---
+++ src/Console/RequestMakeCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Emanci\LumenRequest;
+
+class RequestMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:request';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new form request class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Request';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/stubs/make/request.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Http\Requests';
+    }
+}
